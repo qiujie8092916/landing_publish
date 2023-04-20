bbfcbec49c9c7054f9cc623cdcf966344c1f91b8
jie.q <qiujieee_empty@outlook.com>
2023-04-20 21:42:26 +0800

    :mag: tit

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Landing</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
